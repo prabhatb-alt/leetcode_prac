6b12ce684085d94e65bf63843c466c913260b296
Prabhat Bhatia <prabhat@Prabhats-MacBook-Pro.local>
2026-01-28 10:40:05 +0530

    Problem 1. Two Sum

---
+++ 1. TwoSum.py
@@ -0,0 +1,37 @@
+# Two Sum: LeetCode Problem #1
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.   
+
+# Approach 1: Brute Force
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                
+# Time Complexity: O(n^2): Slow for larger inputs
+
+# Approach 2: Using Hash Map
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        for i in range(len(nums)):
+            check = target - nums[i]
+            if check in dic:
+                return [dic[check], i]
+            dic[nums[i]] = i
+
+# Time Complexity: O(n): Efficient for larger inputs
